214f99a965554cd0da661d3797ec8243257b53c4
Kulakov Ilya <kulakov@selectel.ru>
2023-03-22 10:12:57 +0300

    github workflows
    * add github linter workflow

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+)
+
+var Config *AppConfig
+
+type AppConfig struct {
+	NOAAURL      string `yaml:"noaa_url"`
+	VMMetricsURL string `yaml:"vm_metrics_url"`
+}
+
+func ReadFromFile(path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	if err := loadString(data); err != nil {
+		return err
+	}
+
+	log.Printf("Config loaded from: %s", path)
+
+	return nil
+}
+
+func loadString(data []byte) error {
+	cfg := AppConfig{}
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return err
+	}
+
+	Config = &cfg
+
+	return nil
+}
